658d5b59c9f7c5125153122fd64f87ee3abf491f
faelxvr <faelxvr@outlook.com>
2023-07-26 15:58:53 -0300

    tada: initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toogleMode() {
+  const body = document.body
+
+  if(body.classList.contains('light')) {
+    body.classList.remove('light')
+  } else {
+    body.classList.add('light')
+  }
+}
